f8a0952890aed98de11fc6fd8ae1cb1b1cc9935c
PyankovArt <piankov.ai@phystech.edu>
2022-02-11 09:27:19 +0300

    работа с вектором

---
+++ 2 seminar/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<vector>
+
+
+int main() {
+    std::vector<int> numbers;
+    for (auto i=0; i<30;i++){
+        numbers.push_back(i);
+        std::cout<<"Size is "<<numbers.size()<<" Capacity is "<< numbers.capacity()<<std::endl;
+    }
+    return 0;
+}
+
+/*
+ * std::vector -- динамически меняет размер; добавление в конец О(1); удаление, добавление в середину и начало О(n); по умолчанию
+ * std::string -- не совсем контейнер
+ * std::array -- как вектор, но статический (фиксированный размер); лежит на стеке; используется редко
+ * std::deque -- как вектор, но добавляем и в начало за О(1)
+ *
+ * std::vector
+ * Объект -- сущность, обладающая поведением и состоянием
+ * Поля вектора(состояние): data -- данные; size -- размер занятого; capacity -- ёмкость
+ * Поведение: push_back() -- добавление в конец, resize() -- изменить size
+ */
